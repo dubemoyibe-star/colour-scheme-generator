f5ca3a94b84ae882fb80f93dd0b93e8cb8751879
dubemoyibe-star <dubemoyibe@gmail.com>
2025-10-24 22:19:12 +0100

    made changes

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("get-btn").addEventListener("click", function(){
+    const colorPickerEl = document.getElementById("color-picker").value.substring(1)
+    const modePickerEl = document.getElementById("mode-picker").value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPickerEl}&mode=${modePickerEl}&count=5`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error("Fetch error:", err))
+})
